956d5d76f0d379a6a41cc517b48c85b3b56f97d3
fesuarezj <fesuarezj@gmail.com>
2022-03-17 22:36:58 -0500

    Se crea la estructura de archivos del proyecto, utilizando Node.js+Express - MVC -

---
+++ Proyecto-ZonaGamer/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require("path");
+const app = express()
+const port = 3000
+
+////////////////////////////////////////////////////////////////////////////
+//  Se declara la ruta de los recursos estaticos del proyecto: /public  ///
+//////////////////////////////////////////////////////////////////////////
+const publicPath = path.resolve(__dirname, './public');
+app.use( express.static(publicPath) );
+
+/////////////////////////////////////////////////////////////
+//  Se inicia el servidor express en el puerto definido  ////
+/////////////////////////////////////////////////////////////
+app.listen(port, ()=> {
+    console.log(`Servidor Express ejecutando Proyecto -ZonaGamer- en el puerto : ${port}`);
+});
+
+///////////////////////////////////////////////////////////////////
+////  Bloque donde se definen las Rutas del proyecto ZonaGamer  ///
+///////////////////////////////////////////////////////////////////
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
+app.get('/registro', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/registro.html'));
+});
+
+
