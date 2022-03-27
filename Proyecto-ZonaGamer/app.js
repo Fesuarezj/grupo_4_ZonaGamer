d01949c3594bbf953938dab95be2031ff9819960
scherpablo <scherpablo@gmail.com>
2022-03-26 21:05:45 -0300

    creando pagina de home

--- Proyecto-ZonaGamer/app.js
+++ Proyecto-ZonaGamer/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require("path");
 const app = express()
-const port = 3000
+const port = 3040
 
 ////////////////////////////////////////////////////////////////////////////
 //  Se declara la ruta de los recursos estaticos del proyecto: /public  ///
