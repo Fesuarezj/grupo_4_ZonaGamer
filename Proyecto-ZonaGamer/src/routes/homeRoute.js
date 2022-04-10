0ed902834a25b15a84b7f1a98becb9bfb5da3d3f
scherpablo <scherpablo@gmail.com>
2022-04-10 13:22:23 -0300

    ordenando estructuras de archivos

--- Proyecto-ZonaGamer/src/routes/homeRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const homeController = require('../controllers/homeController');
-
-// se crean las rutas /home, /registro, etc
-router.get('/', homeController.home);
-
-module.exports = router; 
