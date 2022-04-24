661d06ad8c7205cd7440a02d6113916b9a7768f4
scherpablo <scherpablo@gmail.com>
2022-04-24 17:08:30 -0300

    aplicando funcionalidad detalle producto

--- Proyecto-ZonaGamer/src/controllers/productsControllers.js
+++ Proyecto-ZonaGamer/src/controllers/productsControllers.js
@@ -1,13 +1,61 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsData.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
 const productsControllers = {
     producto: (req, res) => {
-        res.render('../views/products/detalleProducto.ejs')
+        const productsFilePath = path.join(__dirname, '../data/productsData.json');
+		const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+        const productoBuscado = products.find(producto => producto.id == req.params.id);
+        const precioFinal = productoBuscado.price - (productoBuscado.price * (productoBuscado.discount / 100));	
+
+        res.render('../views/products/detalleProducto.ejs', {producto: productoBuscado, precio_final: precioFinal})
     },
+
+    
     carrito: (req, res) => {
         res.render('../views/products/carrito.ejs')
     },
+
+/*** AGREGAR PRODUCTO ***/
     agregarProducto: (req, res) => {
-        res.render('../views/products/agregarProducto.ejs', { title: 'Agregar Producto' });
+        // const productsFilePath = path.join(__dirname, '../data/productsData.json');
+        // const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+        res.render('../views/products/agregarProducto.ejs');        
+    },
+
+
+    store: (req, res) => {
+        const productsFilePath = path.join(__dirname, '../data/productsData.json');
+        const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+        const nuevoProducto = {
+        "id": products[products.length - 1].id + 1,//dentro de prodcuts voy a la última posicón del array y a la propiedad id le sumo 1, para almacenar un nuevo producto
+        "name": req.body.name,
+        "description": req.body.description,	
+        "image": req.files[0].filename,
+        "category": req.body.category,
+        "warranty": Number(req.body.warranty),
+        "price": Number(req.body.price),
+        "discount": Number(req.body.discount),
+        "date": req.body.date,
+        "status": req.body.status      
     }
+
+        products.push(nuevoProducto);
+
+		fs.writeFileSync(productsFilePath,JSON.stringify(products, null , ' '));
+		
+		res.redirect('/');
+
+    }
+
 };
 
 
--- Proyecto-ZonaGamer/src/routes/products.js
+++ Proyecto-ZonaGamer/src/routes/products.js
@@ -1,10 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const multer = require('multer');
+const path = require('path');
 
 const productsControllers = require('../controllers/productsControllers');
 
-router.get('/detalle', productsControllers.producto);
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, '../public/images');
+    },
+    filename: (req, file, cb) => {
+        // console.log(file);
+        const nuevoNombreImagen = file.fieldname + '-' + Date.now() + path.extname(file.originalname); 
+        // console.log(nuevoNombreImagen);
+        cb(null, nuevoNombreImagen);
+    }
+});
+
+const uploadFile = multer({ storage: storage });
+
+/*** DETALLE PRODUCTO ***/
+router.get('/detalle/:id/', productsControllers.producto);
+
+
+/*** CARRITO PRODUCTO ***/
 router.get('/carrito', productsControllers.carrito);
+
+/*** AGREGAR PRODUCTO ***/
 router.get('/agregar', productsControllers.agregarProducto);
+router.post('/', uploadFile.any(), productsControllers.store); 
 
 module.exports = router;
