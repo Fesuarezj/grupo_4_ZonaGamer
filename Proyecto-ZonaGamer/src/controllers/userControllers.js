af66ba9fd76087758503b4883ac3ce982e3481a9
scherpablo <scherpablo@gmail.com>
2022-05-18 21:07:57 -0300

    Haciendo Validaciones

--- Proyecto-ZonaGamer/src/controllers/userControllers.js
+++ Proyecto-ZonaGamer/src/controllers/userControllers.js
@@ -1,13 +1,30 @@
 const path = require('path');
 
+const { validationResult } = require('express-validator');
+
 
 
 const userControllers = {
     registro: (req, res) => {
         res.render('../views/users/registro.ejs')
     },
+    procesoRegistro: (req, res) => {
+        const resultValidation = validationResult(req);
+                
+        if (resultValidation.errors.length > 0) {
+            return res.render('../views/users/registro', {
+                errors: resultValidation.mapped(),
+                oldData: req.body
+            });
+        }
+
+        return res.send('Ok, las validaciones fueron correctas');
+    },
     login: (req, res) => {    
         res.render('../views/users/login.ejs')
+    },
+    listaDeUsuarios: (req, res) => {
+        res.render('../views/users/usersList.ejs')
     },
     contacto: (req, res) => {
         res.render('../views/users/contacto.ejs')
--- Proyecto-ZonaGamer/src/routes/users.js
+++ Proyecto-ZonaGamer/src/routes/users.js
@@ -1,10 +1,65 @@
 var express = require('express');
 var router = express.Router();
+const multer = require('multer');
+const path = require('path');
 
 const userControllers = require('../controllers/userControllers');
 
+const { body } = require('express-validator');
+
+const validations = [
+    body('imagenPerfil').custom((value, { req }) =>{
+        let file = req.file;
+        let acceptedExtensions = ['.jpg', '.png', '.gif']
+        
+        if (!file) {
+            throw new Error('Tienes que subir una imagen');
+        } else {
+            let fileExtension = path.extname(file.originalname)
+            if (!acceptedExtensions.includes(fileExtension)) {
+                throw new Error(`Las extensiones de archivo pemitidas son ${acceptedExtensions.join(',')}`);
+            } 
+        }              
+        return true;
+    }),
+    body('nombre').notEmpty().withMessage('Debes poner tu nombre'),
+    body('apellido').notEmpty().withMessage('Debes poner tu apellido'),
+    body('userName').notEmpty().withMessage('Crea un nombre de usuario'),
+    body('correoElectronico')
+    .notEmpty().withMessage('Debes poner un correo').bail()
+    .isEmail().withMessage('Debes escribir un formaro de correo váliddo'),
+    body('contrasenia')
+    .notEmpty().withMessage('Debes poner una contraseña').bail()
+    .isLength({min: 8}).withMessage('Debe contener al menos 8 caracteres')
+]
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, '../public/images/avatars');
+    },
+    filename: (req, file, cb) => {
+        
+        const avatars  = `${Date.now()}_img${path.extname(file.originalname)}`; 
+        
+        cb(null, avatars);
+    }
+});
+
+const uploadFile = multer({ storage: storage });
+
+//FORMULARIO DE REGISTRO
 router.get('/registro', userControllers.registro);
+
+//PROCESAR REGISTRO
+router.post('/registro', uploadFile.single('imagenPerfil'), validations, userControllers.procesoRegistro);
+
+//FORMULARIO DE LOGIN
 router.get('/login', userControllers.login);
+
+//LISTA DE USUARIOS
+router.get('/usersList', userControllers.listaDeUsuarios);
+
+//FORMULARIO DE CONTACTO
 router.get('/contacto', userControllers.contacto);
 
 
