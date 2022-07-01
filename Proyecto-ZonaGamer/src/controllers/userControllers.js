c7db77b08011f4cc23eb5fd20984df815f9e2c84
scherpablo <scherpablo@gmail.com>
2022-07-01 01:42:53 -0300

    Continuando con validaciones

--- Proyecto-ZonaGamer/src/controllers/userControllers.js
+++ Proyecto-ZonaGamer/src/controllers/userControllers.js
@@ -1,6 +1,5 @@
 const bcryptjs = require('bcryptjs');
 const {validationResult} = require('express-validator');
-const path = require('path');
 
 const db = require('../database/models');
 
@@ -15,36 +14,68 @@ const userControllers = {
             return res.render('../views/users/registro.ejs', { 
                 errors: resultValdiation.mapped(),
                 oldData: req.body
-            });    
+            });
+            
+        
+
+
         } else {
-                db.Users.create({
-                    nombre: req.body.nombre,
-                    apellido: req.body.apellido,
-                    correoElectronico: req.body.correoElectronico,
-                    userName: req.body.userName,
-                    // contrasenia: req.body.contrasenia,
-                    contrasenia: bcryptjs.hashSync(req.body.contrasenia, 10),
-                    imagenPerfil: req.file.filename,
-                    rol_ID_rol: 2, //todo usuario registrado y/o creado tendra el rol de cliente
-                    estado_ID_estado: 1 //todo usuario registrado y/o creado tendra el estado activo
-                });
-                return res.render('../views/users/login.ejs')
+            // const project = db.Users.findOne({ where: { userName: req.body.userName } });
+            // // console.log('***********gfgfgddgfdgdfgdf***********fdgfdgfdgfdgdf*', project)
+            
+            // if (project === null) {
+            //     console.log('*****Not found!*******');
+            // } else {
+            //     console.log('*******usuario encontrado********', project);                
+            // }
+            db.Users.findOne({
+                where: {
+                    userName: req.body.userName,                  
+                }
+            })
+            .then(function(users) {
+                console.log('ALERTA USER NAME REPERIDO')
+                return res.render('../views/users/registro.ejs', { 
+                    errors: {
+                        userName: {
+                            msg: 'Este nombre de usuario ya esta registrado'
+                        }
+                    },
+                    oldData: req.body,                    
+                });                                            
+            });
+
+            // db.Users.create({
+            //         nombre: req.body.nombre,
+            //         apellido: req.body.apellido,
+            //         correoElectronico: req.body.correoElectronico,
+            //         userName: req.body.userName,
+            //         // contrasenia: req.body.contrasenia,
+            //         contrasenia: bcryptjs.hashSync(req.body.contrasenia, 10),
+            //         imagenPerfil: req.file.filename,
+            //         rol_ID_rol: 2, //todo usuario registrado y/o creado tendra el rol de cliente
+            //         estado_ID_estado: 1 //todo usuario registrado y/o creado tendra el estado activo
+            //     });
+            //     return res.render('../views/users/login.ejs')
         }            
     },
     login: (req, res) => { 
-        console.log(req.session)
         return res.render('../views/users/login.ejs');
-    },    
-    procesoLogin: (req, res) => {   
+    },       
+    procesoLogin: (req, res) => {           
+        const resultValdiation = validationResult(req);
         
-        // const resultValdiation = validationResult(req);
-        
-        // if(resultValdiation.errors.length > 0) {
-        //     return res.render('../views/users/login.ejs', { 
-        //         errors: resultValdiation.mapped(),
-        //         oldData: req.body
-        //     });    
-        // }          
+        console.log('0holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', req.body.userName)
+        console.log('0holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', req.body.contrasenia)
+
+        if(resultValdiation.errors.length > 0) {
+            return res.render('../views/users/login.ejs', { 
+                errors: resultValdiation.mapped(),
+                oldData: req.body
+            });    
+        } else{         
+        console.log('1holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', req.body.userName)
+        console.log('1holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', req.body.contrasenia)
         
             db.Users.findOne({
                 where: {
@@ -55,7 +86,10 @@ const userControllers = {
             .then(function(users) {
                 const rolId = ['administrador', 'cliente', 'invitado']                   
                 return res.render('../views/users/perfil.ejs', {users: users, rolId: rolId});                             
-            });           
+            });
+        }  
+        console.log('2holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', req.body.userName)
+        console.log('2holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', req.body.contrasenia)         
     },
     perfil: (req, res) => {    
         return res.render('../views/users/perfil.ejs');
--- Proyecto-ZonaGamer/src/middlewares/validateLoginMiddleware.js
+++ Proyecto-ZonaGamer/src/middlewares/validateLoginMiddleware.js
@@ -1,22 +1,7 @@
-const path = require('path');
+
 const { body } = require('express-validator');
 
-module.exports = [
-	body('imagenPerfil').custom((value, { req }) =>{
-        const file = req.file;
-        const acceptedExtensions = ['.jpg', '.png', '.gif']
-        
-        if (!file) {
-            throw new Error('Tienes que subir una imagen');
-        } else {
-            const fileExtension = path.extname(file.originalname)            
-
-            if (!acceptedExtensions.includes(fileExtension)) {
-                throw new Error(`Las extensiones de archivo pemitidas son ${acceptedExtensions.join(', ')}`);
-            } 
-        }              
-        return true;
-    }),    
+module.exports = [   
     body('userName').notEmpty().withMessage('Debes ingresar un nombre de usuario'),    
     body('contrasenia')
     .notEmpty().withMessage('Debes ingresar una contraseña').bail()
