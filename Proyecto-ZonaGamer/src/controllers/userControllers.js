76b1e7b6307b4a6fa5cbc3e0666a57038087ca2f
scherpablo <scherpablo@gmail.com>
2022-07-03 23:14:42 -0300

    Actualizacion 03-07-22

--- Proyecto-ZonaGamer/src/controllers/userControllers.js
+++ Proyecto-ZonaGamer/src/controllers/userControllers.js
@@ -28,35 +28,35 @@ const userControllers = {
             // } else {
             //     console.log('*******usuario encontrado********', project);                
             // }
-            db.Users.findOne({
-                where: {
-                    userName: req.body.userName,                  
-                }
-            })
-            .then(function(users) {
-                console.log('ALERTA USER NAME REPERIDO')
-                return res.render('../views/users/registro.ejs', { 
-                    errors: {
-                        userName: {
-                            msg: 'Este nombre de usuario ya esta registrado'
-                        }
-                    },
-                    oldData: req.body,                    
-                });                                            
-            });
+            // db.Users.findOne({
+            //     where: {
+            //         userName: req.body.userName,                  
+            //     }
+            // })
+            // .then(function(users) {
+            //     console.log('ALERTA USER NAME REPERIDO')
+            //     return res.render('../views/users/registro.ejs', { 
+            //         errors: {
+            //             userName: {
+            //                 msg: 'Este nombre de usuario ya esta registrado'
+            //             }
+            //         },
+            //         oldData: req.body,                    
+            //     });                                            
+            // });
 
-            // db.Users.create({
-            //         nombre: req.body.nombre,
-            //         apellido: req.body.apellido,
-            //         correoElectronico: req.body.correoElectronico,
-            //         userName: req.body.userName,
-            //         // contrasenia: req.body.contrasenia,
-            //         contrasenia: bcryptjs.hashSync(req.body.contrasenia, 10),
-            //         imagenPerfil: req.file.filename,
-            //         rol_ID_rol: 2, //todo usuario registrado y/o creado tendra el rol de cliente
-            //         estado_ID_estado: 1 //todo usuario registrado y/o creado tendra el estado activo
-            //     });
-            //     return res.render('../views/users/login.ejs')
+            db.Users.create({
+                    nombre: req.body.nombre,
+                    apellido: req.body.apellido,
+                    correoElectronico: req.body.correoElectronico,
+                    userName: req.body.userName,
+                    // contrasenia: req.body.contrasenia,
+                    contrasenia: bcryptjs.hashSync(req.body.contrasenia, 10),
+                    imagenPerfil: req.file.filename,
+                    rol_ID_rol: 2, //todo usuario registrado y/o creado tendra el rol de cliente
+                    estado_ID_estado: 1 //todo usuario registrado y/o creado tendra el estado activo
+                });
+                return res.render('../views/users/login.ejs')
         }            
     },
     login: (req, res) => { 
